4290cccc828180f94e1f7802cf8985445ab647d0
juanemmanuelpd <juanemmanuelpd@gmail.com>
2025-06-17 02:13:05 -0600

    First Commit

---
+++ src/TokenBazaar.sol
@@ -0,0 +1,121 @@
+// License
+// SPDX-License-Identifier: MIT
+
+// Solidity Compiler Version
+pragma solidity 0.8.24;
+
+// Libraries
+import "../lib/openzeppelin-contracts/contracts/access/Ownable.sol";
+import "../lib/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
+import "../lib/openzeppelin-contracts/contracts/utils/ReentrancyGuard.sol";
+import "../lib/openzeppelin-contracts/contracts/utils/Pausable.sol";
+
+// Contract
+contract tokenBazaar is ReentrancyGuard, Ownable, Pausable{
+
+    // Variables
+    mapping(address => mapping(uint256 => Listing)) public listing;
+    mapping (address => bool) public blocked;
+    uint256 public percentFees = 5;
+    uint256 public fees;
+    uint256 public sellerAmount;
+    address private bazaarOwner;
+    
+
+    // Structs
+    struct Listing{
+        address seller;
+        address nftaddress;
+        uint256 tokenId;
+        uint256 price;
+    }
+
+    // Events
+    event e_NFTListed(address indexed seller, address indexed nftAddress, uint256 indexed tokenId, uint256 price);
+    event e_NFTCancelled(address indexed seller, address indexed nftAddress, uint256 indexed tokenId);
+    event e_NFTSold(address indexed buyer, address indexed seller, address indexed nftAddress, uint256 tokenId, uint256 price);
+    event e_addToBlackList(address user_, bool blocked);
+    event e_removeFromBlackList(address user_, bool blocked);
+    event e_newFees(uint256 fees);
+
+    // Modifiers
+    modifier notBloked(){
+        require (!blocked[msg.sender],"Blocked user");
+        _;    
+    }
+
+    // Constructor
+
+    constructor() Ownable(msg.sender){
+        bazaarOwner = owner();
+    }
+
+    // Functions
+
+    function listNFT(address nftAddress_, uint256 tokenId_, uint256 price_) external whenNotPaused nonReentrant {
+        require(price_ > 0, "Price can not be 0");
+        address owner_ = IERC721(nftAddress_).ownerOf(tokenId_);
+        require(owner_ == msg.sender, "You are not the owner of the NFT");
+        Listing memory listing_ = Listing({
+            seller: msg.sender,
+            nftaddress: nftAddress_,
+            tokenId: tokenId_,
+            price: price_
+        });
+        listing[nftAddress_][tokenId_] = listing_;
+        emit e_NFTListed(msg.sender, nftAddress_, tokenId_, price_);
+    }
+    
+    function buyNFT(address nftAddress_, uint256 tokenId_) external payable whenNotPaused nonReentrant {
+        Listing memory listing_ = listing[nftAddress_][tokenId_];
+        require(listing_.price > 0, "Listing not exists");
+        require(msg.value == listing_.price, "Incorrect amount");
+        delete listing[nftAddress_][tokenId_];
+        fees = (msg.value/100) * percentFees;
+        sellerAmount = msg.value - fees;
+        (bool success2, ) = listing_.seller.call{value: sellerAmount}("");
+        require(success2, "Fail payment");
+        (bool success, ) = bazaarOwner.call{value: fees}("");
+        require(success, "Failed fee payment");
+        fees = 0;
+        IERC721(nftAddress_).safeTransferFrom(listing_.seller, msg.sender, listing_.tokenId);
+        emit e_NFTSold(msg.sender, listing_.seller, listing_.nftaddress, listing_.tokenId, listing_.price);
+    }
+
+    function modifyFees(uint256 newPercent_) external whenNotPaused onlyOwner{
+        require(newPercent_ > 0 && newPercent_ < 100, "The percent is incorrect");
+        percentFees = newPercent_;
+        emit e_newFees(percentFees);
+    }
+
+    function cancelList(address nftAddress_, uint256 tokenId_) external whenNotPaused nonReentrant {
+        Listing memory listing_ = listing[nftAddress_][tokenId_];
+        require(listing_.seller == msg.sender, "You are not the listing owner");
+        delete listing[nftAddress_][tokenId_];
+        emit e_NFTCancelled(msg.sender, nftAddress_, tokenId_);
+    } 
+
+    function addToBlackList (address user_) external whenNotPaused onlyOwner { 
+        require(!blocked[user_],"Blacklisted");
+        blocked[user_] = true;
+        emit e_addToBlackList (user_, blocked[user_]);
+    }
+        
+   function removeFromBlacklist (address user_) external whenNotPaused onlyOwner { 
+        require(blocked[user_],"Off the list");
+        blocked[user_] = false;
+        emit e_removeFromBlackList(user_, blocked[user_]);
+    }
+
+    function pausetransactions() public onlyOwner {
+        require (!paused(), "The contract is paused");
+        _pause();
+    }
+
+    function unpausetransactions() public onlyOwner (){
+        require (paused(), "The contract is unpaused");
+        _unpause();
+    }
+
+
+}
---
+++ test/TokenBazaar.t.sol
@@ -0,0 +1,194 @@
+// License
+// SPDX-License-Identifier: MIT
+
+// Solidity Compiler Version
+pragma solidity 0.8.24;
+
+// Libraries
+import "forge-std/Test.sol";
+import "../lib/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
+import "../src/TokenBazaar.sol";
+
+// Contracts
+contract mockNFT is ERC721{
+
+    // Constructor
+    constructor() ERC721("MockNFT", "MNFT"){
+        
+    }
+
+    // Functions
+    function mint(address to_, uint256 tokenId_) external {
+        _mint(to_, tokenId_);
+    }
+
+}
+
+contract tokenBazaarTest is Test {
+
+    tokenBazaar tokenBazaarTesting;
+    mockNFT mockNFTTesting;
+    address owner = vm.addr(1);
+    address user = vm.addr(2);
+    uint256 tokenId = 0;
+
+    function setUp() public {
+        vm.startPrank(owner);
+        tokenBazaarTesting = new tokenBazaar();
+        mockNFTTesting = new mockNFT();
+        vm.stopPrank();
+        
+        vm.startPrank(user);
+        mockNFTTesting.mint(user, tokenId);
+        vm.stopPrank();
+    }
+
+    function testMintNFT() public view  {
+        address ownerNFT = mockNFTTesting.ownerOf(tokenId);
+        assert(ownerNFT == user);
+    }
+
+    function testShouldRevertIfPriceIsZero() public {
+        vm.startPrank(user);
+        vm.expectRevert("Price can not be 0");
+        tokenBazaarTesting.listNFT(address(mockNFTTesting), tokenId, 0);
+        vm.stopPrank();
+    }
+
+    function testShouldRevertIfNotOwner() public {
+        vm.startPrank(user);
+        address user2_ = vm.addr(3);
+        uint256 tokenId_ = 1;
+        mockNFTTesting.mint(user2_, tokenId_);
+        vm.expectRevert("You are not the owner of the NFT");
+        tokenBazaarTesting.listNFT(address(mockNFTTesting), tokenId_, 1);
+        vm.stopPrank();
+    }
+
+    function testListNFTCorrectly() public{
+        vm.startPrank(user);
+        (address sellerBefore,,,) = tokenBazaarTesting.listing(address(mockNFTTesting), tokenId);
+        tokenBazaarTesting.listNFT(address(mockNFTTesting), tokenId, 1);
+        (address sellerAfter,,,) = tokenBazaarTesting.listing(address(mockNFTTesting), tokenId);
+        assert(sellerBefore == address(0) && sellerAfter == user);
+        vm.stopPrank();
+    }
+    
+    function testCancelListShouldRevertIfNotOwner() public {
+        vm.startPrank(user);
+        (address sellerBefore,,,) = tokenBazaarTesting.listing(address(mockNFTTesting), tokenId);
+        tokenBazaarTesting.listNFT(address(mockNFTTesting), tokenId, 1);
+        (address sellerAfter,,,) = tokenBazaarTesting.listing(address(mockNFTTesting), tokenId);
+        assert(sellerBefore == address(0) && sellerAfter == user);
+        vm.stopPrank();
+
+        address user2 = vm.addr(3);
+        vm.startPrank(user2);
+        vm.expectRevert("You are not the listing owner");
+        tokenBazaarTesting.cancelList(address(mockNFTTesting), tokenId);
+        vm.stopPrank();
+    }
+
+    function testCancelListShouldWorkCorrectly() public {
+        vm.startPrank(user);
+        (address sellerBefore,,,) = tokenBazaarTesting.listing(address(mockNFTTesting), tokenId);
+        tokenBazaarTesting.listNFT(address(mockNFTTesting), tokenId, 1);
+        (address sellerAfter,,,) = tokenBazaarTesting.listing(address(mockNFTTesting), tokenId);
+        assert(sellerBefore == address(0) && sellerAfter == user);
+        tokenBazaarTesting.cancelList(address(mockNFTTesting), tokenId);
+        (address sellerAfter2,,,) = tokenBazaarTesting.listing(address(mockNFTTesting), tokenId);
+        assert(sellerAfter2 == address(0));
+        vm.stopPrank();
+    }
+
+    function testCanNotBuyUnlistedNFT() public {
+        address user2 = vm.addr(3);
+        vm.startPrank(user2);
+        vm.expectRevert("Listing not exists");
+        tokenBazaarTesting.buyNFT(address(mockNFTTesting), tokenId);
+        vm.stopPrank();
+    }
+
+    function testCanNotBuyWithIncorrectPay() public {
+        vm.startPrank(user);
+        uint256 price = 1e18;
+        (address sellerBefore,,,) = tokenBazaarTesting.listing(address(mockNFTTesting), tokenId);
+        tokenBazaarTesting.listNFT(address(mockNFTTesting), tokenId, price);
+        (address sellerAfter,,,) = tokenBazaarTesting.listing(address(mockNFTTesting), tokenId);
+        assert(sellerBefore == address(0) && sellerAfter == user);
+        vm.stopPrank();
+
+        address user2 = vm.addr(3);
+        vm.startPrank(user2);
+        vm.deal(user2, price);
+        vm.expectRevert("Incorrect amount");
+        tokenBazaarTesting.buyNFT{value: price -1}(address(mockNFTTesting), tokenId);
+        vm.stopPrank();
+    }
+
+    function testShouldBuyNFTCorrectly() public {
+        vm.startPrank(user);
+        uint256 price = 1e18;
+        (address sellerBefore,,,) = tokenBazaarTesting.listing(address(mockNFTTesting), tokenId);
+        tokenBazaarTesting.listNFT(address(mockNFTTesting), tokenId, price);
+        (address sellerAfter,,,) = tokenBazaarTesting.listing(address(mockNFTTesting), tokenId);
+        assert(sellerBefore == address(0) && sellerAfter == user);
+        mockNFTTesting.approve(address(tokenBazaarTesting), tokenId);
+        vm.stopPrank();
+
+        address user2 = vm.addr(3);
+        vm.startPrank(user2);
+        vm.deal(user2, 2e18);
+        uint256 balanceBefore = address(user).balance;
+        address ownerBefore = mockNFTTesting.ownerOf(tokenId);
+        (address sellerBefore2,,,) = tokenBazaarTesting.listing(address(mockNFTTesting), tokenId);
+        tokenBazaarTesting.buyNFT{value: price}(address(mockNFTTesting), tokenId);
+        (address sellerAfter2,,,) = tokenBazaarTesting.listing(address(mockNFTTesting), tokenId);   
+        address ownerAfter = mockNFTTesting.ownerOf(tokenId);  
+        uint256 balanceAfter = address(user).balance;
+        assert(sellerBefore2 == user && sellerAfter2 == address(0));
+        assert(ownerBefore == user && ownerAfter == user2);
+        assert(balanceAfter == balanceBefore + tokenBazaarTesting.sellerAmount());
+        vm.stopPrank();
+    }
+
+    function testOwnerCanModifyPercentFeesCorrectly() public{
+       vm.startPrank(owner);
+       uint256 percentBefore = tokenBazaarTesting.percentFees();
+       uint256 newPercent = 30;
+       tokenBazaarTesting.modifyFees(newPercent);
+       uint256 percentAfter =  tokenBazaarTesting.percentFees();
+       assert(percentAfter != percentBefore);
+       vm.stopPrank(); 
+    }
+
+    function testOwnerCanAddToBlackListCorrectly() public{
+       vm.startPrank(owner);
+       tokenBazaarTesting.addToBlackList(user);
+       assert(tokenBazaarTesting.blocked(user) == true);
+       vm.stopPrank(); 
+    }
+
+    function testOwnerCanRemoveFromBlackListCorrectly() public{
+       vm.startPrank(owner);
+       tokenBazaarTesting.addToBlackList(user);
+       tokenBazaarTesting.removeFromBlacklist(user);
+       assert(tokenBazaarTesting.blocked(user) == false);
+       vm.stopPrank(); 
+    }
+
+    function testOwnerCanPauseTheContract() public{
+       vm.startPrank(owner);
+       tokenBazaarTesting.pausetransactions();
+       assert(tokenBazaarTesting.paused());
+       vm.stopPrank(); 
+    }
+
+    function testOwnerCanUnpauseTheContract() public{
+       vm.startPrank(owner);
+       tokenBazaarTesting.pausetransactions();
+       tokenBazaarTesting.unpausetransactions();
+       assert(!tokenBazaarTesting.paused());
+       vm.stopPrank(); 
+    }
+}
